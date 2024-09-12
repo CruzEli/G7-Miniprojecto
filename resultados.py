a5e8665f457cdc37f6403e301fbb7c61d620cac2
Epsil-Astra <83615454+CruzEli@users.noreply.github.com>
2024-09-11 22:29:17 -0300

    Ventana de Resultados
    creacion de una ventana de resultados que muestra las estadisticas del juego. por ahora muestra las preguntas hechas, las correctas e incorrectas

--- resultados.py
+++ resultados.py
@@ -2,16 +2,31 @@ import tkinter as tk
 
 
 class Resultados():
-    def __init__(self, vent):
+    def __init__(self, vent, cnt, cnt_bien, cnt_mal):
         self.vent = vent
         self.vent.state('zoomed')
         self.vent.resizable(False, False)
         self.vent.config(bg="#ffcc99")
-        self.label = tk.Label(self.vent, text="Resultados")
-        self.label.pack()
-        
+        self.label_1 = tk.Label(self.vent, text="Resultados")
+        self.label_1.pack()
+
+
+        self.contador = cnt
+        self.label_2 = tk.Label(self.vent, text=f"Preguntas hechas:{self.contador}")
+        self.label_2.pack()
+
+        self.correctas = cnt_bien
+        self.label_3 = tk.Label(self.vent, text=f"Preguntas Correctas:{self.correctas}")
+        self.label_3.pack()
+
+        self.incorrectas = cnt_mal
+        self.label_4 = tk.Label(self.vent, text=f"Preguntas Incorrectas:{self.incorrectas}")
+        self.label_4.pack()
+
         self.btn = tk.Button(self.vent, text="Salir", command=lambda: self.salir())
         self.btn.pack()
+
+
     
     def salir(self):
         self.vent.destroy()
